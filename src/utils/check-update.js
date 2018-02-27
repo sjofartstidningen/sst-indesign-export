e58e9a2fa98a2be2edf6cbf0c7f605709ab98ada
Adam Bergman <adam@fransvilhelm.com>
2018-02-27 10:05:29 +0100

    docs: Add inline documentation

--- src/utils/check-update.js
+++ src/utils/check-update.js
@@ -7,11 +7,23 @@ import pkg from '../../package.json';
 /**
  * Parse a version string (e.g. v1.0.0) into an array of [major, minor, patch]
  *
- * @param {any} v
+ * @param {String}  v  Semver string
  */
 const parseVersion = v =>
   map(n => Number.parseInt(n, 10), v.replace(/\D/g, '').split(''));
 
+/**
+ * Check if a version is greater than another version, accepting two strings.
+ *
+ * The strings can contain additional chars (e.g. "v" or anything else) as long
+ * as three numbers can be extracted. Return true if v1 is greater than v2.
+ *
+ * Otherwise it will return false.
+ *
+ * @param {String}  v1  Semver string
+ * @param {String}  v2  Semver string
+ * @returns boolean
+ */
 const versionGreaterThan = (v1, v2) => {
   const ver1 = parseVersion(v1);
   const ver2 = parseVersion(v2);
@@ -30,7 +42,17 @@ const versionGreaterThan = (v1, v2) => {
   return false;
 };
 
-const actionWindow = ({ name, label, action }) => {
+/**
+ * Create a window with an actionable button.
+ *
+ * @param {Object}   config
+ * @param {String}   config.name         Name of window
+ * @param {String}   config.label        Label text
+ * @param {String}   config.actionButton Button label text
+ * @param {Function} config.action       Action to take on click
+ * @returns Number? Returns 2 if user clicks cancel, otherwis undefined
+ */
+const actionWindow = ({ name, label, actionButton, action }) => {
   const window = new Window('dialog', name, undefined, {
     independent: true,
   });
@@ -42,11 +64,7 @@ const actionWindow = ({ name, label, action }) => {
   const buttonGroup = window.add('group');
   buttonGroup.alignment = 'right';
 
-  const downloadBtn = buttonGroup.add(
-    'button',
-    undefined,
-    i18n('Download update'),
-  );
+  const downloadBtn = buttonGroup.add('button', undefined, actionButton);
   buttonGroup.add('button', undefined, i18n('Cancel'), { name: 'cancel' });
 
   downloadBtn.addEventListener('click', () => {
@@ -57,6 +75,14 @@ const actionWindow = ({ name, label, action }) => {
   return window.show();
 };
 
+/**
+ * Download an asset from github and place it at the same location as the
+ * current running script.
+ *
+ * @param {Object} asset
+ * @param {String} asset.url  Url to download asset from github
+ * @param {String} asset.name Name of the file to write to
+ */
 function downloadAsset({ url, name }) {
   const headers = `-H 'Accept: application/octet-stream' -H 'User-Agent: ${
     pkg.name
@@ -81,7 +107,22 @@ function downloadAsset({ url, name }) {
   newScript.close();
 }
 
+/**
+ * This function will check if an update is available at the moment.
+ * The check will only occur once a month.
+ *
+ * The store param should have the methods "get" and "set" to retrieve and
+ * define configurations. It is used to see when the last check for an update
+ * was made, and also update it once a check was mad succesfully.
+ *
+ * @param {ConfigStore} store ConfigStore instance
+ * @returns undefined
+ */
 function checkForUpdate(store) {
+  /**
+   * Check if an update has been made the last month.
+   * If so, return early to avoid unneccesary http requests.
+   */
   const lastCheck = store.get('lastUpdateCheck') || 0;
   const currentDate = Date.now();
 
@@ -111,6 +152,7 @@ function checkForUpdate(store) {
       label: i18n(
         'A new version of this script is available, would you like to download it?',
       ),
+      actionButton: i18n('Download update'),
       action: () => downloadAsset(asset),
     });
 
