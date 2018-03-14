faeaa7868f66a20a69177da6ec40a2e196e2738a
Adam Bergman <adam@fransvilhelm.com>
2018-03-14 13:08:00 +0100

    fix(polyfills): Add polyfills for Object.keys and Object.entries

--- src/polyfills/index.js
+++ src/polyfills/index.js
@@ -19,3 +19,63 @@ if (!String.prototype.trim) {
     return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
   };
 }
+
+if (!Object.keys) {
+  Object.keys = (function() {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+      hasDontEnumBug = !{ toString: null }.propertyIsEnumerable('toString'),
+      dontEnums = [
+        'toString',
+        'toLocaleString',
+        'valueOf',
+        'hasOwnProperty',
+        'isPrototypeOf',
+        'propertyIsEnumerable',
+        'constructor',
+      ],
+      dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (
+        typeof obj !== 'function' &&
+        (typeof obj !== 'object' || obj === null)
+      ) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [],
+        prop,
+        i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  })();
+}
+
+if (!Object.entries) {
+  Object.entries = function(obj) {
+    const ownProps = Object.keys(obj);
+    let i = ownProps.length;
+    const resArray = new Array(i);
+
+    while (i--) {
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    }
+
+    return resArray;
+  };
+}
