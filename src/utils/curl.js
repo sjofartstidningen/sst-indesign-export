ea0c7a196c75394acfe38f84cab3fed088cc5594
Adam Bergman <adam@fransvilhelm.com>
2018-03-14 13:09:02 +0100

    fix: Extract curl command in own module
    This method accepts an url and then an options object with headers.

---
+++ src/utils/curl.js
@@ -0,0 +1,25 @@
+import pkg from '../../package.json';
+
+const defaultHeaders = {
+  'User-Agent': `{pkg.name}/${pkg.version}`,
+};
+
+function curl(url, { headers = {} } = {}) {
+  const mergedHeaders = Object.etries({
+    ...defaultHeaders,
+    ...headers,
+  })
+    .map(([key, value]) => `-H '${key}: ${value}'`)
+    .join(' ');
+
+  const command = `curl ${mergedHeaders} ${url} -L`;
+  const response = app.doScript(
+    'return do shell script item 1 of arguments',
+    ScriptLanguage.applescriptLanguage,
+    [command],
+  );
+
+  return response;
+}
+
+export { curl as default };
